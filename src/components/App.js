782dcc2e52a7314bcd3e6022805a56f6b56b7962
Denis Frolov <denisfrolov.coder@gmail.com>
2021-05-29 01:21:31 +0300

    added a get request for a complete list of authors for the book entered/added the ability to enter the title of the book in the search bar/added output to the page of cards with search results for a query in the search bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,73 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+
+import styles from "./Add.module.css";
 
 class App extends Component {
-    render(){
-        return(
-            <div>
-                <h1>Hello, React!</h1>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      book: "Dostoevsky",
+      data: [],
+    };
+    this.getResources();
+  }
+
+  async getResources() {
+    await axios({
+      url: "https://openlibrary.org/search.json",
+      params: {
+        q: `${this.state.book}`,
+      },
+    })
+      .then((res) => {
+        this.setState((state) => {
+          return {
+            data: [...res.data.docs],
+          };
+        });
+      })
+      .catch((err) => console.error(err.JSON()));
+  }
+
+  enterNameBook = (e) => {
+    this.setState((state) => {
+      return {
+        book: e.target.value,
+      };
+    });
+    this.getResources();
+  };
+
+  render() {
+    return (
+      <div>
+        <div className={styles.search}>
+          <input
+            className={styles.search_input}
+            type="text"
+            value={this.state.book}
+            placeholder="Enter name author book..."
+            onChange={this.enterNameBook}
+          />
+        </div>
+
+        <div>
+          <ul className={styles.list}>
+            {this.state.data.map((item) =>
+              item.author_name ? (
+                <li key={item._version_} className={styles.item}>
+                  <p>Image: {item.cover_i ? item.cover_i : "Not image"}</p>
+                  <p>Title: {item.title ? item.title : "Not title"}</p>
+                  <p>Name: {item.author_name}</p>
+                </li>
+              ) : null
+            )}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
